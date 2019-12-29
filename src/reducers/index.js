52a0522d4eb63985a18a17866bb97dc9c1459f86
takiguchi-yu <y.takiguti@gmail.com>
2019-12-30 00:28:49 +0900

    create reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,25 @@
+const events = (state = [], action) => {
+  
+  switch(action.type) {
+
+    // イベント作成
+    case 'CREATE_EVENT': 
+      const event = { title: action.title, body: action.body };
+      const length = state.length;
+      const id = length === 0 ? 1 : state[length - 1].id + 1;
+
+      return [...state, { id, ...event }];
+
+    // イベント削除
+    case 'DELETE_EVENT': 
+      return state;
+
+    // 全てのイベント削除
+    case 'DELETE_ALL_EVENT': 
+      return [];
+    default:
+      return state;  
+  }
+};
+
+export default events;
