8ebbe37e0b2052b60c267d36331a0f05f5b52e7d
takiguchi-yu <8340629+takiguchi-yu@users.noreply.github.com>
2020-01-04 09:30:09 +0900

    create operationLogs reducer

---
+++ src/reducers/operationLogs.js
@@ -0,0 +1,20 @@
+import { ADD_OPERATION_LOG, DELETE_ALL_OPERATION_LOG } from '../actions'
+
+const operationLogs = (state = [], action) => {
+  switch(action.type) {
+
+    case ADD_OPERATION_LOG:
+      const operationLog = {
+        description: action.description,
+        operatedAt: action.operatedAt
+      }
+
+      return [operationLog, ...state]
+    case DELETE_ALL_OPERATION_LOG:
+      return state
+    default:
+      return state
+  }
+}
+
+export default operationLogs
