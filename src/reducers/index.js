02b7eed03ebe1bb1df9002ebef59eb17b3bac3cc
takiguchi-yu <8340629+takiguchi-yu@users.noreply.github.com>
2020-01-04 09:04:05 +0900

    refactor reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,27 +1,4 @@
-import {CREATE_EVENT, DELETE_EVENT, DELETE_ALL_EVENT} from '../actions'
+import { combineReducers } from 'redux';
+import events from './events';
 
-const events = (state = [], action) => {
-  
-  switch(action.type) {
-
-    // イベント作成
-    case CREATE_EVENT: 
-      const event = { title: action.title, body: action.body };
-      const length = state.length;
-      const id = length === 0 ? 1 : state[length - 1].id + 1;
-
-      return [...state, { id, ...event }];
-
-    // イベント削除
-    case DELETE_EVENT:
-      return state.filter(event => event.id !== action.id);
-
-    // 全てのイベント削除
-    case DELETE_ALL_EVENT: 
-      return [];
-    default:
-      return state;  
-  }
-};
-
-export default events;
+export default combineReducers({ events });
